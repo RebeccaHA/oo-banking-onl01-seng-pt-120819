481baf7f8956eb1b91945e0fee758d1cfa8e43c3
calm-block-7153 <rebeccahunt034@gmail.com>
2020-01-14 23:35:51 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,13 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status, :amount, :bank_account
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+  
+  def valid?
+    
 end
